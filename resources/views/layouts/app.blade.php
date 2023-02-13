2e50221a8798e8b200677dde18af2f5ce110bbfd
DarioRossi99 <rssdra99@gmail.com>
2023-02-13 17:05:04 +0100

    .

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,7 +27,7 @@
             <div class="container">
                 <a class="navbar-brand d-flex align-items-center" href="{{ url('/') }}">
                     <div class="logo_laravel">
-                        Paolo Lo Re
+                        me dead
                     </div>
                     {{-- config('app.name', 'Laravel') --}}
                 </a>
